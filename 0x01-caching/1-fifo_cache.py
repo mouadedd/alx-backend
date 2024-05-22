58c150172fac39de3ff39fd359cc09c998e22abb
mouadedd <mdx.edd@gmail.com>
2024-05-22 04:53:22 -0700

    added a space on the discard message

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -18,7 +18,7 @@ class FIFOCache(BaseCaching):
         else:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS and\
                     key not in self.cache_data:
-                print(f"DISCARD:{self.cache_list[0]}")
+                print(f"DISCARD: {self.cache_list[0]}")
                 del self.cache_data[self.cache_list[0]]
                 del self.cache_list[0]
             self.cache_list.append(key)
