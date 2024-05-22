6448d851ad0a639f05f8034e78bbc034f4b64e7c
mouadedd <mdx.edd@gmail.com>
2024-05-22 04:07:30 -0700

    to test

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """0. Basic dictionary"""
-
 BaseCaching = __import__('base_caching').BaseCaching
 
 
@@ -8,14 +7,17 @@ class BasicCache(BaseCaching):
     """inherits from BaseCaching and is a caching system"""
 
     def __init__(self):
+        """initialize class using parent"""
         BaseCaching.__init__(self)
 
     def put(self, key, item):
+        """put a key-value"""
         if key is None or item is None:
             pass
         self.cache_data[key] = item
 
     def get(self, key):
+        """get key's value"""
         if key is None or key not in self.cache_data.keys():
             return None
         return self.cache_data[key]
