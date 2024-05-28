0daaed3b4d7a70f43e0d4926c608260300308bd6
mouadedd <mdx.edd@gmail.com>
2024-05-28 12:17:28 -0700

    race

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""Basic Flask app"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+from typing import Dict, Union
+
+
+class Config(object):
+    """ app configuration """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """determine the best match with our supported languages."""
+    ways = [
+            request.args.get('locale'),
+            g.user.get('locale', None) if g.user else None,
+            request.accept_languages.best_match(app.config['LANGUAGES']),
+            Config.BABEL_DEFAULT_LOCALE
+            ]
+    for locale in ways:
+        if locale and locale in app.config['LANGUAGES']:
+            return locale
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user(id) -> Union[Dict[str, Union[str, None]], None]:
+    """user login details or none"""
+    return users.get(int(id), 0)
+
+
+@app.before_request
+def before_request():
+    """ find user if any and set it to global"""
+    setattr(g, 'user', get_user(request.args.get('login_as', 0)))
+
+
+@app.route('/')
+def index() -> str:
+    """ render html file"""
+    return render_template('6-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
