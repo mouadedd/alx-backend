197bcf3b1555d22da01d1b6f203e6f2787651a32
mouadedd <mdx.edd@gmail.com>
2024-05-28 07:17:50 -0700

    doc

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index() -> str:
-    """ render html """
-    return render_template("index.html")
+    """ render html file"""
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
