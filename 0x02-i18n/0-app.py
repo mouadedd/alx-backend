4df82fe207b49c17ec4f8ec49be7a2b1a22ce48d
mouadedd <mdx.edd@gmail.com>
2024-05-28 07:19:47 -0700

    fixed template name

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -3,13 +3,13 @@
 from flask import Flask, render_template
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def index() -> str:
     """ render html file"""
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == "__main__":
