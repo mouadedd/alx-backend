cfb90c18d8dbce806177e54864986f02f53501bf
mouadedd <mdx.edd@gmail.com>
2024-05-28 11:33:25 -0700

    imported request

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
@@ -18,7 +18,7 @@ babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """determine the best match with our supported languages."""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
@@ -18,10 +18,10 @@ babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """determine the best match with our supported languages."""
-    locale = request.args.get('locale', '').strip()
-    if locale and locale in Config.LANGUAGES:
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
