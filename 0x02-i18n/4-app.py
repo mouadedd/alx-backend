8bc046492f494a61d234c5ef01e299c1a81f884f
mouadedd <mdx.edd@gmail.com>
2024-05-28 11:36:11 -0700

    folder

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -11,7 +11,7 @@ class Config(object):
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 app.config.from_object(Config)
 
 babel = Babel(app)
